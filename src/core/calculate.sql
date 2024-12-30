615a7d4a5906a8aedb005bf49e4e257896bf870a
Slava Rozhnev <rozhnev@msn.com>
2024-12-30 15:07:57 +0200

    Create calculate.sql
    Calculation examples SQL code

---
+++ src/core/calculate.sql
@@ -0,0 +1,7 @@
+select 2 + 3; -- simple calculation
+
+select 60 * 60 * 24; -- calculate seconds in 24 hours
+
+select PI() * POW(5, 2); -- using functions
+
+select current_date, current_time;  -- current date and time
