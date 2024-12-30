3470e5597ac3c00e730acb0a59830748b101e7b3
Slava Rozhnev <rozhnev@msn.com>
2024-12-30 15:58:18 +0200

    Update calculate.sql
    Added strings concatenation example

--- src/core/calculate.sql
+++ src/core/calculate.sql
@@ -2,6 +2,8 @@ select 2 + 3; -- simple calculation
 
 select 60 * 60 * 24; -- calculate seconds in 24 hours
 
+select 'Happy ' || 'New Year!'; -- strings concatenation
+
 select PI() * POW(5, 2); -- using functions
 
 select current_date, current_time;  -- current date and time
